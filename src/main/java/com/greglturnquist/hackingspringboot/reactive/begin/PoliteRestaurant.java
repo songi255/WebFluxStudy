08a15a936dfa458c96bdd9e417fe99aaaa88708a
songi255 <songi255@github.com>
2022-11-15 23:44:34 +0900

    docker..

--- src/main/java/com/greglturnquist/hackingspringboot/reactive/begin/PoliteRestaurant.java
+++ src/main/java/com/greglturnquist/hackingspringboot/reactive/begin/PoliteRestaurant.java
@@ -1,7 +1,7 @@
 package com.greglturnquist.hackingspringboot.reactive.begin;
 
 public class PoliteRestaurant {
-    public static void main(String... args){
+    /*public static void main(String... args){
         PoliteServer server = new PoliteServer(new KitchenService());
 
         server.doingMyJob().subscribe(
@@ -10,5 +10,5 @@ public class PoliteRestaurant {
         );
         // 구독을 했다.. 드디어 무언가 작동한다. subscribe() 하기 전까지는 절대 아무일도 일어나지 않는다!!!
         // project reactor는 태생적으로 lazy 방식이다. 커넥션도, 웹요청처리도, 웹필터도 활성화되지 않는다.
-    }
+    }*/
 }
